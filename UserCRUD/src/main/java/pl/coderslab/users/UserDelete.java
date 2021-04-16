e2adb0ba962c60229a927cfd9ac605f8f5707d29
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2021-04-16 17:35:54 +0200

    Servlet CRUD

--- UserCRUD/src/main/java/pl/coderslab/users/UserDelete.java
+++ UserCRUD/src/main/java/pl/coderslab/users/UserDelete.java
@@ -1,19 +1,18 @@
 package pl.coderslab.users;
 
+import pl.coderslab.entity.UserDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "UserDelete", value = "/UserDelete")
+@WebServlet(name = "UserDelete", value = "/user/delete")
 public class UserDelete extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        UserDao userDao = new UserDao();
+        userDao.deleteUser(Integer.parseInt(request.getParameter("id")));
+        response.sendRedirect(request.getContextPath() + "/user/list");
     }
 }
--- UserCRUD/src/main/java/pl/coderslab/users/UserList.java
+++ UserCRUD/src/main/java/pl/coderslab/users/UserList.java
@@ -1,2 +1,21 @@
-package pl.coderslab.users;public class UserList {
+package pl.coderslab.users;
+
+import pl.coderslab.entity.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/list")
+public class UserList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserDao userDao = new UserDao();
+        request.setAttribute("users", userDao.findAllUsers());
+        getServletContext().getRequestDispatcher("/users/list.jsp").forward(request, response);
+
+    }
 }
